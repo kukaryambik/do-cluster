3566029c0dad0e84df012072300dcb2f09a58c6e
Aleksandr Iakimenko <kukaryambik@gmail.com>
2025-03-15 00:44:28 +0200

    update init.sh and sts_cosmos.yaml

--- cluster/cosmos/cm_init-sh/init.sh
+++ cluster/cosmos/cm_init-sh/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Set up a Gaia service to join the provider chain.
 
-NODE_HOME=~/.gaia
+NODE_HOME=$HOME
 NODE_MONIKER=$(hostname)
 GAS_PRICE=0.005uatom
 CHAIN_BINARY='gaiad'
@@ -12,17 +12,17 @@ SNAPSHOT_URL=https://snapshots.polypore.xyz/ics-testnet/provider/latest.tar.gz
 
 # Initialize home directory
 echo "Initializing $NODE_HOME..."
-rm -rf $NODE_HOME
-$CHAIN_BINARY config set client chain-id $CHAIN_ID --home $NODE_HOME
-$CHAIN_BINARY config set client keyring-backend test --home $NODE_HOME
-$CHAIN_BINARY init "$NODE_MONIKER" --chain-id $CHAIN_ID --home $NODE_HOME
-sed -i -e "/minimum-gas-prices =/ s^= .*^= \"$GAS_PRICE\"^" $NODE_HOME/config/app.toml
-sed -i -e "/seeds =/ s^= .*^= \"$SEEDS\"^" $NODE_HOME/config/config.toml
+rm -rf "$NODE_HOME"
+$CHAIN_BINARY config set client chain-id $CHAIN_ID --home "$NODE_HOME"
+$CHAIN_BINARY config set client keyring-backend test --home "$NODE_HOME"
+$CHAIN_BINARY init "$NODE_MONIKER" --chain-id $CHAIN_ID --home "$NODE_HOME"
+sed -i -e "/minimum-gas-prices =/ s^= .*^= \"$GAS_PRICE\"^" "$NODE_HOME/config/app.toml"
+sed -i -e "/seeds =/ s^= .*^= \"$SEEDS\"^" "$NODE_HOME/config/config.toml"
 
 echo "Downloading snapshot..."
-rm -rf $NODE_HOME/wasm $NODE_HOME/data
-wget -qO- $SNAPSHOT_URL | tar vxz -C $NODE_HOME
+rm -rf "$NODE_HOME/wasm" "$NODE_HOME/data"
+wget -qO- $SNAPSHOT_URL | tar vxz -C "$NODE_HOME"
 
 # Replace genesis file
 wget $GENESIS_URL -O /tmp/genesis.json
-mv /tmp/genesis.json $NODE_HOME/config/genesis.json
+mv /tmp/genesis.json "$NODE_HOME/config/genesis.json"
